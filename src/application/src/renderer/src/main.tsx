4c82e578942b23512e17481784c108390d47d465
enso <71034993+itiva@users.noreply.github.com>
2024-11-02 23:53:24 +0200

    Adding react flow

--- src/application/src/renderer/src/main.tsx
+++ src/application/src/renderer/src/main.tsx
@@ -9,6 +9,8 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+import '@xyflow/react/dist/style.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
